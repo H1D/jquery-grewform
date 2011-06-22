5363772ce4554b270c3c006101a5a51e301adc06
Artem Suschev <Suschev.Artem+github@gmail.com>
2011-06-22 04:34:39 +0400

    some upgrades

--- jquery.grewform.js
+++ jquery.grewform.js
@@ -17,13 +17,22 @@ jQuery.fn.grewform = function(options){
             (function(form,rule,eventObj){
                  var wait = setInterval(
                      function() {
-                         if( !form.children().is(":animated"))
+                         if( !form.find().is(":animated"))
                          {
                              clearInterval(wait);
-                             if(rule.trigged && form.children(rule.selector).length == 0)
+                             if(rule.trigged && form.find(rule.selector).length == 0)
                                  rule.run_unmatch_actions()
-                             else if((!rule.trigged) && form.children(rule.selector+':visible').length > 0)
-                                 rule.run_match_actions(eventObj)
+                             else if((!rule.trigged))
+                             {
+                                 //('option:visible') always return nothing
+                                 if(form.find(rule.selector).filter('option').length > 0)
+                                 {
+                                    if(form.find(rule.selector).parent('select:visible').length > 0)
+                                        rule.run_match_actions()
+                                 }
+                                 else if(form.find(rule.selector).filter(':visible').length > 0)
+                                    rule.run_match_actions()
+                             }
                          }
                      },
                      200
@@ -43,13 +52,16 @@ function Rule(selector,form,raw_rule){
     {
         //unique id for rules
         Rule.id=0
-        //special blip to mark DOM elements witch become visible by this rule
-        Rule.blip_ptr = 'ololo_rule'
+        //special blip to mark DOM elements witch invoke rule actions
+        Rule.blip_ptr = 'rule'
         //function for cascade unmatch
         Rule.unmtach_by_blip = function(blip){
                                    for(var k in Rule.all)
                                        if(Rule.all[k].blip === blip)
+                                       {
+                                           debug('unmtach_by_blip '+blip)
                                            Rule.all[k].run_unmatch_actions()
+                                       }
                                }
 
         //All rules (hello Django:])
@@ -59,10 +71,11 @@ function Rule(selector,form,raw_rule){
     this.id = Rule.id++
     this.blip = Rule.blip_ptr+this.id
     this.selector = selector          //selector of the rule
-    this.trigged = false       //indicates is rule trigged or not on current form
+    this.trigged = false       //indicates is rule trigged or not
     this.match_actions = []
     this.unmatch_actions = []
     this.raw = raw_rule
+    this.form = form
 
     //extract actions
     for(var action_key in raw_rule)
@@ -70,11 +83,12 @@ function Rule(selector,form,raw_rule){
 
     debug('created rule: '+this.selector +'['+ this.match_actions.length +'|'+this.unmatch_actions.length+']')
 
-    this.run_match_actions = function(eventObj){
+    this.run_match_actions = function(){
         debug('match: '+this.selector)
 
         this.trigged = true
-        jQuery(eventObj.target).addClass(this.blip)
+        //mark elements that rule trigged, it's need for cascade actions
+        this.form.find(this.selector).addClass(this.blip)
         for(var c in this.match_actions)
             this.match_actions[c].call(this)
     }
@@ -142,7 +156,7 @@ function generate_actions(key,form,rule)
                         function(e,selector,v,h){
                              return function(){
                                  debug('add_options action')
-                                 var selects = e.children(selector)
+                                 var selects = e.find(selector)
                                  jQuery('<option></option>').html(h)
                                               .val(v)
                                               .appendTo(selects);
@@ -154,7 +168,7 @@ function generate_actions(key,form,rule)
                              return function(){
                                  debug('add_options action rollback');
                                  cascade_unmatch(e.children(selector).children('option[value='+v+']'))
-                                 e.children(selector).children().remove('option[value='+v+']')
+                                 e.find(selector).children().remove('option[value='+v+']')
                              }
                         }(form,selector,v)
                     )
@@ -183,5 +197,6 @@ function cascade_unmatch(elements)
         jQuery.each(classes,function(k,v){
             Rule.unmtach_by_blip(v)
         })
+        cascade_unmatch(elements.children())
     })
 }
